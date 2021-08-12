71cd0a553ddef281187c24827eb61f06edd45817
sumitsingh4411 <shiv6371913458@gmail.com>
2021-08-12 15:08:40 +0530

    make it better

--- src/firbase/firebase.js
+++ src/firbase/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";;
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCgWduqXEmmFoOTzKV4GFJd_aIEvYX_8Dk",
-    authDomain: "fleet-point-311715.firebaseapp.com",
-    projectId: "fleet-point-311715",
-    storageBucket: "fleet-point-311715.appspot.com",
-    messagingSenderId: "1042119208019",
-    appId: "1:1042119208019:web:6a4d4bf9037475854d2aa6",
-    measurementId: "G-138W173KNJ"
+    // add your firebase configuration
   };
   const fire=firebase.initializeApp(firebaseConfig);
   const db=fire.firestore();
